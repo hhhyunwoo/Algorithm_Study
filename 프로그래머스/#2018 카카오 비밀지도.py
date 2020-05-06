6a377fed55898cb9c4f25c624e2849163529cdf4
Hyunwoo Kim <37402136+qrlagusdn@users.noreply.github.com>
2020-05-06 15:31:40 +0900

    2018 카카오 비밀지도

---
+++ 프로그래머스/#2018 카카오 비밀지도.py
@@ -0,0 +1,25 @@
+#2018 카카오 비밀지도
+
+def solution(n, arr1, arr2):
+    answer = []
+    print(str(bin(arr1[0])))
+    for i in range(0,n):
+        tmp1,tmp2 = str(bin(arr1[i]))[2:],str(bin(arr2[i]))[2:]
+        if len(tmp1) <n:
+            tmp = "0"*(n-len(tmp1))
+            tmp1 = tmp+tmp1
+        if len(tmp2) <n:
+            tmp = "0"*(n-len(tmp2))
+            tmp2 = tmp+tmp2    
+        tmp3 = ""
+        for j in range(0,n):
+            if tmp1[j] == '0' and tmp2[j]=='0': #둘다 공백
+                tmp3 +=' '
+            else:
+                tmp3 += '#'
+        answer.append(tmp3)
+    return answer
+
+if __name__ == "__main__":
+    print(solution(5,[9, 20, 28, 18, 11],[30, 1, 21, 17, 28]))
+
